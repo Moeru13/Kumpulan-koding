ce016756f69edeaa3db1d84017c1ded7029aa1bd
Moeru13 <brianmangile13@gmail.com>
2023-12-02 12:18:49 +0800

    Revisi Akhir

--- Kelompok 5.py
+++ Kelompok 5.py
@@ -1,37 +1,65 @@
 # Manajemen Keuangan 
-# - Pencatatan Income # - Tampilkan Riwayat Transaksi # - Hitung Saldo Akhir
+# - Pencatatan Keuangan 
+# - Tampilkan Riwayat Transaksi 
+# - Hitung Saldo Akhir
 
 def keterangan():
     print("---------------------------------------------")
-    print("| A. Pencatatan Income Dan Outcome          |")
-    print("| B. Tampilkan Riwayat Transaksi            |")
-    print("| C. Hitung Saldo Akhir                     |")
-    print("| D. Keluar                                 |")
+    print("| 1. Pencatatan Pemasukkan Dan Pengeluaran  |")
+    print("| 2. Tampilkan Riwayat Transaksi            |")
+    print("| 3. Hitung Saldo Akhir                     |")
+    print("| 4. Keluar                                 |")
     print("---------------------------------------------")
+
+def selesai():
+    print("-"*45)
+    print("Terimakasih Atas Kepercayaan Kepada Kami".center(45,' '))
+    print("Semoga Kelak Anda Jadi Orang Kaya".center(45,' '))
+    print("AMIN".center(45,' '))
+    print("-"*45)
+    exit()
 
 income  = []
 outcome = []
 
 while True :
+    # Menampilkan Menu
     keterangan()
-    pilihan = input("Masukkan Pilihan Anda : ").upper()
-    if pilihan == "A":
+    pilihan = input("Masukkan Pilihan Anda : ")
+
+    # - Pencatatan Keuangan
+    if pilihan == "1":
         pemasukkan  = float(input("Masukkan Pendapatan  : Rp "))
         pengeluaran = float(input("Masukkan Pengeluaran : Rp "))
         income.append(pemasukkan)
         outcome.append(pengeluaran)
         print()
+        print("Keuangan Ada Telah Dicatat".center(45,"-"))
+        print()
 
-    elif pilihan == "B":
-        print("---------------------------------------------")
-        print(" Riwayat Transaksi ".center(45,"="))
-        print("| Pemasukkan  : Rp",sum(income))
-        print("| Pengeluaran : Rp",sum(outcome))
-        print("---------------------------------------------\n")
+        tanya = input("Tekan y untuk masuk ke menu utama : ").lower()
+        if tanya == "y":
+            print()
+        else : 
+            selesai()
 
-    elif pilihan == "C":
+    # - Tampilkan Riwayat Transaksi 
+    elif pilihan == "2":
+        for nomor,pemasukkan in enumerate(income):
+            print(" Riwayat Transaksi ".center(45, "="))
+            print(f"{nomor+1} | Pemasukkan  : Rp{pemasukkan}\n  | Pengeluaran : Rp{outcome[nomor]}")
+            print("-"*45)
+            print()
+        tanya = input("Tekan y untuk masuk ke menu utama : ").lower()
+        if tanya == "y":
+            print()
+        else : 
+            selesai()
+
+    # - Hitung Saldo Akhir
+    elif pilihan == "3":
         saldo = sum(income) - sum(outcome)
-        print("---------------------------------------------")
+        print(" Total Saldo ".center(45, "="))
         print("| Total Pemasukan   : Rp",sum(income))
         print("| Total Pengeluaran : Rp",sum(outcome))
         print("| Total Saldo       : Rp",saldo)
@@ -42,20 +70,22 @@ while True :
             print("---------------------------------------------\n")
 
         elif saldo == 0 :
-            print(" Kasian T Ada Perubahan ".center(45, '='))
+            print(" Kasian T Ada Perubahan ".center(45, '+'))
             print("---------------------------------------------\n")
 
         else :
-            print(" Kase Banya Jo Itu Utang ".center(45, '='))
+            print(" Kase Banya Jo Itu Utang ".center(45, '+'))
             print("--------------------------------------------\n")
 
-    elif pilihan == "D":
-        print("-"*45)
-        print("Terimakasih Atas Kepercayaan Kepada Kami".center(45,' '))
-        print("Semoga Kelak Anda Jadi Orang Kaya".center(45,' '))
-        print("AMIN".center(45,' '))
-        print("-"*45)
-        exit()
+        tanya = input("Tekan y untuk masuk ke menu utama : ").lower()
+        if tanya == "y":
+            print()
+            continue
+        else : 
+            selesai()
+
+    elif pilihan == "4":
+        selesai()
 
     else:
         print ("Mohon Masukkan Inputan Yang Sesuai")
